f61e2f328dce61fc73ca50671943a4e78c415a90
ranniex <deporsam@hotmail.com>
2016-11-08 11:44:30 -0500

    Add files via upload

---
+++ tablero.h
@@ -0,0 +1,33 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
+/*
+ * pokemon-crush
+ * Copyright (C) SAMIR FARID VALENCIA VELASCO 2016 <samirfvv@s5pc31>
+ * 
+pokemon-crush is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * pokemon-crush is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _TABLERO_H_
+#define _TABLERO_H_
+
+class Tablero
+{
+public:
+
+protected:
+
+private:
+
+};
+
+#endif // _TABLERO_H_
