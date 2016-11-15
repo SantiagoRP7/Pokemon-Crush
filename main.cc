6c6a83c704e9d4592985a625d5bd88a75105f5fc
sixnoble98 <cristian.manosalva@correounivalle.edu.co>
2016-11-15 15:31:54 -0500

    Add files via upload

--- main.cc
+++ main.cc
@@ -1,14 +1,14 @@
 /* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
 /*
  * main.cc
- * Copyright (C) 2016 SAMIR FARID VALENCIA VELASCO <samirfvv@s5pc31>
+ * Copyright (C) 2016 CRISTIAN MANOSALVA BELTRAN <crismb@s5pc23>
  * 
- * Pokemon-Crush is free software: you can redistribute it and/or modify it
+ * Ejemplocrush is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * Pokemon-Crush is distributed in the hope that it will be useful, but
+ * Ejemplocrush is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
@@ -18,8 +18,61 @@
  */
 
 #include <iostream>
+using namespace std;
+#include "caja.h"
+#include "tablero.h"
 
 int main()
-{
+{	int x,y,x1,y1,op;
+	int t = 0;
+	Tablero matrix1;
+	matrix1.drawmatrix();
+	cout << "----------------\n";
+
+	while(op != -1)
+	{
+		cout << "1 - Consulta \n";
+		cout << "2 - Cambiar numero \n";
+		cout << "3 - rellenar \n";
+		cout << "4 - Dibujar \n";
+		cout << "OP: ";
+		cin >> op;
+		switch(op)
+		{
+			case 1:
+				matrix1.drawmatrix();
+				cout << "x: ";
+				cin >> x;
+				cout << "y: ";
+				cin >> y;
+				//cout << "Pos requerida: "<<matrix1.getnumpos(x,y)<<endl;
+				break;
+			
+			case 2:
+				matrix1.drawmatrix();
+				cout << "x: ";
+				cin >> x;
+				cout << "y: ";
+				cin >> y;
+				cout << "x1: ";
+				cin >> x1;
+				cout << "y1: ";
+				cin >> y1;
+				matrix1.validMove(x,y,x1,y1);
+				break;
+			
+			case 3:
+				matrix1.drawmatrix();
+				matrix1.fillmatrix();
+				cout << "----------------\n";
+				matrix1.drawmatrix();
+				break;
+			case 4:
+				cout << "----------------\n";
+				matrix1.drawmatrix();
+				cout << "----------------\n";
+		}			
+	}
+		
 	return 0;
 }
